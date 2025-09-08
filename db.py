b3ea97d6e552e666aaa680b02a1bade25175d90e
Vera Ama Pomaa <verapomaa99@gmail.com>
2025-09-08 21:23:28 +0000

    Created the db.py file

---
+++ db.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+#Connect to Mongo Atlas Cluster
+mongo_client = MongoClient(os.getenv("MONGO_URI"))
+
+
+# Access database
+advert_management_db = mongo_client["advert_management_db"]
+
+
+# Pick a connection to operate on
+adverts_collection = advert_management_db["adverts"]
